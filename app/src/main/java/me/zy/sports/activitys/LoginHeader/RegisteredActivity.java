6e85d0d06dc5dc3f5b312960ae5c18c22db9c275
Cheungyan <314573840@qq.com>
2019-05-04 06:13:47 +0800

    AAA

---
+++ app/src/main/java/me/zy/sports/activitys/LoginHeader/RegisteredActivity.java
@@ -0,0 +1,24 @@
+package me.zy.sports.activitys.LoginHeader;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.zy.sports.R;
+import me.zy.sports.activitys.BaseActivity;
+
+/**
+ * 项目名：sports
+ * 包名：me.zy.sports.activitys.LoginHeader
+ * Created by Administrator on 2019/5/4.
+ * 描述：
+ */
+  public class RegisteredActivity extends BaseActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registered);
+
+
+
+    }
+}
